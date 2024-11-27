c564787e601b95d7f1c82dcf1139c93028bea815
Chris V <creevue@gmail.com>
2024-11-27 01:56:10 -0500

    TeamInfoController code added, fixed and tested Swagger Post/Get

--- ContemporaryAPI/Controllers/TeamInfoController.cs
+++ ContemporaryAPI/Controllers/TeamInfoController.cs
@@ -1,1 +1,100 @@
-﻿
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ContemporaryAPI.Data;
+using ContemporaryAPI.Models;
+
+namespace ContemporaryAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TeamInfoController : ControllerBase
+    {
+        private readonly ContemporaryDbContext _context;
+
+        public TeamInfoController(ContemporaryDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/TeamInfo
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TeamInfo>>> GetTeamInfos()
+        {
+            return await _context.TeamInfos.ToListAsync();
+        }
+
+        // GET: api/TeamInfo/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TeamInfo>> GetTeamInfo(int id)
+        {
+            var teamInfo = await _context.TeamInfos.FindAsync(id);
+
+            if (teamInfo == null)
+            {
+                return NotFound();
+            }
+
+            return teamInfo;
+        }
+
+        // POST: api/TeamInfo
+        [HttpPost]
+        public async Task<ActionResult<TeamInfo>> PostTeamInfo(TeamInfo teamInfo)
+        {
+            _context.TeamInfos.Add(teamInfo);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetTeamInfo", new { id = teamInfo.Id }, teamInfo);
+        }
+
+        // PUT: api/TeamInfo/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTeamInfo(int id, TeamInfo teamInfo)
+        {
+            if (id != teamInfo.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(teamInfo).State = EntityState.Modified;
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TeamInfoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // DELETE: api/TeamInfo/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTeamInfo(int id)
+        {
+            var teamInfo = await _context.TeamInfos.FindAsync(id);
+            if (teamInfo == null)
+            {
+                return NotFound();
+            }
+
+            _context.TeamInfos.Remove(teamInfo);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool TeamInfoExists(int id)
+        {
+            return _context.TeamInfos.Any(e => e.Id == id);
+        }
+    }
+}
--- ContemporaryAPI/Models/TeamInfo.cs
+++ ContemporaryAPI/Models/TeamInfo.cs
@@ -1,23 +1,31 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace ContemporaryAPI.Models
 {
     public class TeamInfo
     {
-        // Unique identifier(Primary Key)
+        // Unique identifier (Primary Key)
         [Key]
         public int Id { get; set; }
 
         // Full name
+        [Required]
+        [MaxLength(200)]
         public string FullName { get; set; } = string.Empty;
 
         // Birthdate
+        [Required]
         public DateTime Birthdate { get; set; }
 
         // College program: CEAS, CECH, ArtsSci, DAAP, etc
+        [Required]
+        [MaxLength(100)]
         public string CollegeProgram { get; set; } = string.Empty;
 
-        // Freshmen, Sophmore, Junior, Senior
+        // Freshmen, Sophomore, Junior, Senior
+        [Required]
+        [MaxLength(50)]
         public string ProgramYear { get; set; } = string.Empty;
     }
 }
